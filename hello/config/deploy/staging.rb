bc7456b0ebe7edd633046823f9e8182737c73e98
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2015-06-25 00:30:12 +0700

    Config staging

---
+++ hello/config/deploy/staging.rb
@@ -0,0 +1,54 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :domain, '12.34.567.89'
+set :deploy_to, '/home/root/hello'
+set :repository, 'git@github.com:vinhngl/setup-and-run-rails.git'
+set :branch, 'master'
+set :forward_agent, true
+set :port, 22
+set :user, 'root'
+set :identity_file, '/path/to/ssh/key'
+set :rails_env, :staging
+set :rbenv_path, '/usr/local/rbenv'
+
+set :shared_paths, ['config/database.yml', 'log', 'config/secrets.yml']
+
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/secrets.yml'."]
+
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  to :before_hook do
+  end
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'rbenv:load'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+    end
+  end
+end
